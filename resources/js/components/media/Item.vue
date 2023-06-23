43db94baafd8cb18c70225b99a3ca6e2395b06a3
huynhanhkhoa2895 <huynhanhkhoa2895@gmail.com>
2023-06-24 01:27:00 +0700

    update

--- resources/js/components/media/Item.vue
+++ resources/js/components/media/Item.vue
@@ -3,8 +3,11 @@
         <img v-if="item.mime_type.startsWith('image')" :class="'h-' + height" class="object-cover w-full" :src="item.conversions.preview" :alt="item.name" />
         <div v-else :class="'h-' + height" class="w-full flex items-center justify-center">
             <icon :type="'m-document'" :width="48" :height="48" class="fill-no-stroke-current" />
+          <div>
             <p class="">{{item.name}}</p>
             <p class="">({{item.mime_type}})</p>
+          </div>
+
         </div>
         <slot></slot>
     </div>
